7a66b199d3c425b604f7465fbb813a41ccf81b25
Алексей Евтух <lexa_1433@mail.ru>
2020-05-22 22:35:45 +0300

    Les 9 finishing project

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -2,14 +2,34 @@ import React from 'react';
 
 import './ItemStatusFilter.css';
 
-const ItemStatusFilter =() => {
+class ItemStatusFilter extends React.Component {
+    
+    buttons = ['all', 'active', 'done'];
+    
+    render() {
+
+        const {onItemsFilterChange, filter} = this.props;
+
+        const buttons = this.buttons.map((el) => {
+            const btnClass = filter === el 
+                ? 'btn-success': 'btn-outline-secondary';
+            return (
+                <button 
+                    key = {el}
+                    className = {`btn ${btnClass}`}
+                    onClick ={() => onItemsFilterChange(el)}>
+                {el}</button>
+            );
+        });
+
         return(
-            <div className = 'Btn-group ItemStatusFilter'>
-                <button className = 'btn btn-success'>All</button>
-                <button className = 'btn btn-outline-secondary'>Active</button>
-                <button className = 'btn btn-outline-secondary'>Done</button>
+            <div className = 'btn-group ItemStatusFilter'>
+                {buttons}
             </div>
-        )
-}
+            );
+        }
+    }    
+    
+    
 
 export default ItemStatusFilter;
--- src/components/SearchBlock/SearchBlock.js
+++ src/components/SearchBlock/SearchBlock.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import './SearchBlock.css';
-import ItemStatusFilter from '../ItemStatusFilter';
 
 
-const SearchBlock = () => {
-    const searchBlockText = 'What do you want to search?'
-    return (
-    <div className = 'SearchBlock d-flex'>
-      <input  
-        className = 'form-control mr-3' 
-        placeholder = {searchBlockText} />
-      <ItemStatusFilter />
-    </div>
-    )}
+
+class SearchBlock extends React.Component {
+  state ={
+    detectedText: '',
+  };
+
+  onSearchChange = (event) =>{
+    const detectedText = event.target.value;
+    this.setState({ detectedText });
+    this.props.onSearchChange(detectedText);
+  }
+    
+    render() {
+      const searchBlockText = 'What do you want to search?';
+
+      return (
+        <div className = 'SearchBlock d-flex'>
+          <input  
+            className = 'form-control mr-3' 
+            placeholder = {searchBlockText}
+            value = {this.state.detectedText} 
+            onChange = {this.onSearchChange}/>
+        </div>
+        );
+      }
+    }
+
+    
 
   export default SearchBlock;
